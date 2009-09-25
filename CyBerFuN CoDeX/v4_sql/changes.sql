081497c8cca20eb2516b141a17a8e87b45cdebb3
cybernet <cybernet2u@2reply.in>
2009-09-25 13:32:29 +0000

    1 bug fixed / http://cyberfun.ro/ / cybernet

--- CyBerFuN CoDeX/v4_sql/changes.sql
+++ CyBerFuN CoDeX/v4_sql/changes.sql
@@ -1,8 +1,8 @@
---- CyBerFuN.Ro
---- By CyBerNe7
----            ---
---- www.cyberfun.ro ---
---- last update 09/06/2009 ( US datetime ) / cybernet / www.cyberfun.ro /
+-- CyBerFuN.Ro
+-- By CyBerNe7
+--            --
+-- www.cyberfun.ro --
+-- last update 09/06/2009 ( US datetime ) / cybernet / www.cyberfun.ro /
 
 ALTER TABLE `sitelog` CHANGE `type` `type` ENUM( 'torrentupload', 'torrentedit', 'torrentdelete', 'promotion', 'demotion', 'addwarn', 'remwarn', 'accenabled', 'accdisabled', 'accdeleted', 'slowautoclean', 'autohitrun', 'autobackupdb', 'autooptimizedb', 'passwordreset', 'ratioedit', 'newmember', 'customsmiles', 'autoclean', 'autowarn', 'autodewarn', 'autoban', 'staffaction', 'shoutcom', 'userdelete', 'cyberfun_clean_comments_from_deleted_torrents', 'rating_table_cleaned', 'thanks_table_cleaned', 'snatched_table_cleaned','log') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'torrentupload' COMMENT 'last update 09/11/2009 ( US datetime ) / cybernet / www.cyberfun.ro /' ;
 
@@ -17,6 +17,7 @@ DELETE FROM `stylesheets` WHERE `stylesheets`.`id` = 8 LIMIT 1;
 DELETE FROM `stylesheets` WHERE `stylesheets`.`id` = 9 LIMIT 1;
 ALTER TABLE `users` CHANGE `title` `title` VARCHAR( 32 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'last update 09/11/2009 ( US datetime ) / cybernet / www.cyberfun.ro /' ;
 DELETE FROM `bonus` WHERE `bonus`.`id` = 5 LIMIT 1;
-ALTER TABLE `topics` CHANGE `subject` `subject` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL 
+ALTER TABLE `topics` CHANGE `subject` `subject` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
+DELETE FROM `staffpanel` WHERE `staffpanel`.`id` = 21 LIMIT 1
 
---- Have FuN on CyBerFuN / cybernet2u
+-- Have FuN on CyBerFuN / cybernet2u
